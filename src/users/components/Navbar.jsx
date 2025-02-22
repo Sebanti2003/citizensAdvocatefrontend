3977d46d460eed897084318aa00af28cc23e0d30
Sebanti2003 <dasguptasebanti2003@gmail.com>
2025-02-23 03:05:17 +0530

    done

--- src/users/components/Navbar.jsx
+++ src/users/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           </button>
 
           {/* CTA Buttons */}
-          <Link to="/user/selectcategory" className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
+          <Link to="/user/login" className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
             📢 File a Complaint
           </Link>
           <Link to="/govt/login" className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700">
